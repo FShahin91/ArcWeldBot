ca6517a32587f6f7b73e762ee37c90a35bcc60b9
Baker Shehadeh <bakermohammad.bm94@gmail.com>
2022-08-29 21:52:05 +0300

    Add cosine movement. Clean the code.

--- scene_and_ctrl/scripts/ctrl.py
+++ scene_and_ctrl/scripts/ctrl.py
@@ -33,6 +33,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # Author: Acorn Pooley, Mike Lautman
+# Modified by: Baker Shehadeh, Faris Shahin, Mahmoud Alqannas
 
 ## BEGIN_SUB_TUTORIAL imports
 ##
@@ -41,7 +42,6 @@
 ## and a `RobotCommander`_ class. More on these below. We also import `rospy`_ and some messages that we will use:
 ##
 
-# Python 2/3 compatibility imports
 from __future__ import print_function
 from six.moves import input
 
@@ -52,17 +52,7 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 
-try:
-    from math import pi, tau, dist, fabs, cos, sin, degrees, radians
-except:  # For Python 2 compatibility
-    from math import pi, fabs, cos, sqrt, sin, degrees, radians
-
-    tau = 2.0 * pi
-
-    def dist(p, q):
-        return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
-
-
+from math import pi, dist, fabs, cos, sin, radians
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 
@@ -104,65 +94,24 @@ class MoveGroupPython(object):
 
     def __init__(self):
         super(MoveGroupPython, self).__init__()
-
-        ## BEGIN_SUB_TUTORIAL setup
-        ##
-        ## First initialize `moveit_commander`_ and a `rospy`_ node:
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
 
-        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-        ## kinematic model and the robot's current joint states
         robot = moveit_commander.RobotCommander()
-
-        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-        ## for getting, setting, and updating the robot's internal understanding of the
-        ## surrounding world:
         scene = moveit_commander.PlanningSceneInterface()
-
-        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        ## to a planning group (group of joints).  In this tutorial the group is the primary
-        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
-        ## If you are using a different robot, change this value to the name of your robot
-        ## arm planning group.
-        ## This interface can be used to plan and execute motions:
         group_name = "arm"
         move_group = moveit_commander.MoveGroupCommander(group_name)
-
-        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-        ## trajectories in Rviz:
         display_trajectory_publisher = rospy.Publisher(
             "/move_group/display_planned_path",
             moveit_msgs.msg.DisplayTrajectory,
             queue_size=20,
         )
 
-        ## END_SUB_TUTORIAL
-
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
         ## Getting Basic Information
         ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-        # We can get the name of the reference frame for this robot:
         planning_frame = move_group.get_planning_frame()
-        #print("============ Planning frame: %s" % planning_frame)
-
-        # We can also print the name of the end-effector link for this group:
         eef_link = move_group.get_end_effector_link()
-        #print("============ End effector link: %s" % eef_link)
-
-        # We can get a list of all the groups in the robot:
         group_names = robot.get_group_names()
-        #print("============ Available Planning Groups:", robot.get_group_names())
-
-        # Sometimes for debugging it is useful to print the entire state of the
-        # robot:
-        #print("============ Printing robot state")
-        #print(robot.get_current_state())
-        #print("")
-        ## END_SUB_TUTORIAL
-
-        # Misc variables
         self.box_name = ""
         self.robot = robot
         self.scene = scene
@@ -172,28 +121,16 @@ class MoveGroupPython(object):
         self.eef_link = eef_link
         self.group_names = group_names
 
-    def go_to_joint_state(self, joint_goal=[]):
+    def go_to_joint_state(self, joint_goal):
         ## Planning to a Joint Goal
         ## ^^^^^^^^^^^^^^^^^^^^^^^^
-        ## The Panda's zero configuration is at a `singularity <https://www.quora.com/Robotics-What-is-meant-by-kinematic-singularity>`_, so the first
-        ## thing we want to do is move it to a slightly better configuration.
-        ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
-        # We get the joint values from the group and change some of the values:
-        #joint_goal = self.move_group.get_current_joint_values()
         if len(joint_goal) != 7:
             print("Number of joints must not be 7.")
             return False
         
         joint_goal[6] = 0
-
-        # The go command can be called with joint values, poses, or without any
-        # parameters if you have already set the pose or joint target for the group
         self.move_group.go(joint_goal, wait=True)
-
-        # Calling ``stop()`` ensures that there is no residual movement
         self.move_group.stop()
-
-        # For testing:
         current_joints = self.move_group.get_current_joint_values()
         return all_close(joint_goal, current_joints, 0.01)
 
@@ -209,11 +146,6 @@ class MoveGroupPython(object):
         ## ^^^^^^^^^^^^^^^^^^^^^^^
         ## We can plan a motion for this group to a desired pose for the
         ## end-effector:
-        #pose_goal = geometry_msgs.msg.Pose()
-        #pose_goal.orientation.w = 1.0
-        #pose_goal.position.x = 0.4
-        #pose_goal.position.y = 0.1
-        #pose_goal.position.z = 0.4
 
         move_group.set_pose_target(pose_goal)
 
@@ -233,143 +165,14 @@ class MoveGroupPython(object):
         current_pose = self.move_group.get_current_pose().pose
         return all_close(pose_goal, current_pose, 0.01)
 
-    def plan_cartesian_path(self, xMove=0, yMove=0, zMove=0):
-
-        ## BEGIN_SUB_TUTORIAL plan_cartesian_path
-        ##
-        ## Cartesian Paths
-        ## ^^^^^^^^^^^^^^^
-        ## You can plan a Cartesian path directly by specifying a list of waypoints
-        ## for the end-effector to go through. If executing  interactively in a
-        ## Python shell, set scale = 1.0.
-        ##
-        waypoints = []
-
-        wpose = self.move_group.get_current_pose().pose
-        wpose.position.x += xMove  # and sideways (y)
-        wpose.position.y += yMove  # and sideways (y)
-        wpose.position.z += zMove  # First move up (z)
-
-        waypoints.append(copy.deepcopy(wpose))
-
-        wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
-        waypoints.append(copy.deepcopy(wpose))
-
-        wpose.position.y -= scale * 0.1  # Third move sideways (y)
-        waypoints.append(copy.deepcopy(wpose))
-
-        # We want the Cartesian path to be interpolated at a resolution of 1 cm
-        # which is why we will specify 0.01 as the eef_step in Cartesian
-        # translation.  We will disable the jump threshold by setting it to 0.0,
-        # ignoring the check for infeasible jumps in joint space, which is sufficient
-        # for this tutorial.
-        (plan, fraction) = self.move_group.compute_cartesian_path(
-            waypoints, 0.01, 0.0  # waypoints to follow  # eef_step
-        )  # jump_threshold
-
-        # Note: We are just planning, not asking move_group to actually move the robot yet:
-        return plan, fraction
-
-        ## END_SUB_TUTORIAL
-
-    def display_trajectory(self, plan):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        robot = self.robot
-        display_trajectory_publisher = self.display_trajectory_publisher
-
-        ## BEGIN_SUB_TUTORIAL display_trajectory
-        ##
-        ## Displaying a Trajectory
-        ## ^^^^^^^^^^^^^^^^^^^^^^^
-        ## You can ask RViz to visualize a plan (aka trajectory) for you. But the
-        ## group.plan() method does this automatically so this is not that useful
-        ## here (it just displays the same trajectory again):
-        ##
-        ## A `DisplayTrajectory`_ msg has two primary fields, trajectory_start and trajectory.
-        ## We populate the trajectory_start with our current robot state to copy over
-        ## any AttachedCollisionObjects and add our plan to the trajectory.
-        display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-        display_trajectory.trajectory_start = robot.get_current_state()
-        display_trajectory.trajectory.append(plan)
-        # Publish
-        display_trajectory_publisher.publish(display_trajectory)
-
-        ## END_SUB_TUTORIAL
-
-    def execute_plan(self, plan):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        move_group = self.move_group
-
-        ## BEGIN_SUB_TUTORIAL execute_plan
-        ##
-        ## Executing a Plan
-        ## ^^^^^^^^^^^^^^^^
-        ## Use execute if you would like the robot to follow
-        ## the plan that has already been computed:
-        move_group.execute(plan, wait=True)
-
-        ## **Note:** The robot's current joint state must be within some tolerance of the
-        ## first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
-        ## END_SUB_TUTORIAL
-
-    def wait_for_state_update(
-        self, box_is_known=False, box_is_attached=False, timeout=4
-    ):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
-
-        ## BEGIN_SUB_TUTORIAL wait_for_scene_update
-        ##
-        ## Ensuring Collision Updates Are Received
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## If the Python node was just created (https://github.com/ros/ros_comm/issues/176),
-        ## or dies before actually publishing the scene update message, the message
-        ## could get lost and the box will not appear. To ensure that the updates are
-        ## made, we wait until we see the changes reflected in the
-        ## ``get_attached_objects()`` and ``get_known_object_names()`` lists.
-        ## For the purpose of this tutorial, we call this function after adding,
-        ## removing, attaching or detaching an object in the planning scene. We then wait
-        ## until the updates have been made or ``timeout`` seconds have passed.
-        ## To avoid waiting for scene updates like this at all, initialize the
-        ## planning scene interface with  ``synchronous = True``.
-        start = rospy.get_time()
-        seconds = rospy.get_time()
-        while (seconds - start < timeout) and not rospy.is_shutdown():
-            # Test if the box is in attached objects
-            attached_objects = scene.get_attached_objects([box_name])
-            is_attached = len(attached_objects.keys()) > 0
-
-            # Test if the box is in the scene.
-            # Note that attaching the box will remove it from known_objects
-            is_known = box_name in scene.get_known_object_names()
-
-            # Test if we are in the expected state
-            if (box_is_attached == is_attached) and (box_is_known == is_known):
-                return True
-
-            # Sleep so that we give other threads time on the processor
-            rospy.sleep(0.1)
-            seconds = rospy.get_time()
-
-        # If we exited the while loop without returning then we timed out
-        return False
-        ## END_SUB_TUTORIAL
-
-
 def main():
     try:
         print("Initializing control...")
         ctrlgroup = MoveGroupPython()
         print("Moving robot to home postion...")
         joint_target = [0, -pi/2, pi/2, 0, pi/2, pi, 0]
-        ctrlgroup.go_to_joint_state(joint_target)
+        if not ctrlgroup.go_to_joint_state(joint_target):
+            print("WARNING: Current pose does not match the plan")
 
         print("Moving robot to position...")
         joint_target = ctrlgroup.move_group.get_current_joint_values()
@@ -380,20 +183,18 @@ def main():
         joint_target[3] = radians(19)
         joint_target[4] = radians(80)
         joint_target[5] = radians(170)
-        ctrlgroup.go_to_joint_state(joint_target)
+        if not ctrlgroup.go_to_joint_state(joint_target):
+            print("WARNING: Current pose does not match the plan")
 
         targetPose = geometry_msgs.msg.Pose()
         tempPose = geometry_msgs.msg.Pose()
         currentPose = ctrlgroup.move_group.get_current_pose()
         targetPose = copy.deepcopy(currentPose.pose)
         tempPose = copy.deepcopy(currentPose.pose)
-        #print("Current Pose is:")
-        #print(currentPose.pose.position.x)
-        #print(currentPose.pose.position.y)
-        #print(currentPose.pose.position.z)
         waypoints = []
         
-        for angle in range (0, 90):
+        print("Welding first circle...")
+        for angle in range (0, 360):
             x = (0.1 * cos(radians(angle))) - 0.1
             y = 0.1 * sin(radians(angle))
             targetPose.position.x = tempPose.position.x - x
@@ -401,10 +202,10 @@ def main():
             waypoints.append(copy.deepcopy(targetPose))
         
         (plan, fraction) = ctrlgroup.move_group.compute_cartesian_path(
-            waypoints, 0.01, 0.0  # waypoints to follow  # eef_step
-        )
-        ctrlgroup.execute_plan(plan)
-
+            waypoints, 0.01, 0.0)
+        ctrlgroup.move_group.execute(plan, wait=True)
+        print("Welding done.")
+        print("Welding second circle...")
         waypoints.clear()
         for angle in range (0, 360):           
             x = 0.1 - (0.1 * cos(radians(angle)))
@@ -414,8 +215,41 @@ def main():
             waypoints.append(copy.deepcopy(targetPose))
         
         (plan, fraction) = ctrlgroup.move_group.compute_cartesian_path(
-            waypoints, 0.01, 0.0  # waypoints to follow  # eef_step
-        )
+            waypoints, 0.01, 0.0)
+        ctrlgroup.move_group.execute(plan, wait=True)
+        print("Welding done.")
+        ########################################################
+        print("Moving arm to new position...")
+        joint_target = ctrlgroup.move_group.get_current_joint_values()
+        joint_target[0] = radians(159)
+        joint_target[1] = radians(-152)
+        joint_target[2] = radians(-53)
+        joint_target[3] = radians(-7)
+        joint_target[4] = radians(106)
+        joint_target[5] = radians(-8)
+
+        if not ctrlgroup.go_to_joint_state(joint_target):
+            print("WARNING: Current pose does not match the plan")
+        print("Robot arm in postion.")
+        targetPose = geometry_msgs.msg.Pose()
+        tempPose = geometry_msgs.msg.Pose()
+        currentPose = ctrlgroup.move_group.get_current_pose()
+
+        targetPose = copy.deepcopy(currentPose.pose)
+        tempPose = copy.deepcopy(currentPose.pose)
+        waypoints.clear()
+        print("Welding a cosine track...")
+        for angle in range (0, 720):
+            y = tempPose.position.y - 0.1 * cos(radians(angle))
+            x = targetPose.position.x - 0.001
+            targetPose.position.x =  x
+            targetPose.position.y =  y
+            waypoints.append(copy.deepcopy(targetPose))
+
+        (plan, fraction) = ctrlgroup.move_group.compute_cartesian_path(
+            waypoints, 0.01, 0.0)
+        ctrlgroup.move_group.execute(plan, wait=True)
+        print("Welding done.")
         print("End of planning.")
         
         
@@ -427,39 +261,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-## BEGIN_TUTORIAL
-## .. _moveit_commander:
-##    http://docs.ros.org/noetic/api/moveit_commander/html/namespacemoveit__commander.html
-##
-## .. _MoveGroupCommander:
-##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1move__group_1_1MoveGroupCommander.html
-##
-## .. _RobotCommander:
-##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1robot_1_1RobotCommander.html
-##
-## .. _PlanningSceneInterface:
-##    http://docs.ros.org/noetic/api/moveit_commander/html/classmoveit__commander_1_1planning__scene__interface_1_1PlanningSceneInterface.html
-##
-## .. _DisplayTrajectory:
-##    http://docs.ros.org/noetic/api/moveit_msgs/html/msg/DisplayTrajectory.html
-##
-## .. _RobotTrajectory:
-##    http://docs.ros.org/noetic/api/moveit_msgs/html/msg/RobotTrajectory.html
-##
-## .. _rospy:
-##    http://docs.ros.org/noetic/api/rospy/html/
-## CALL_SUB_TUTORIAL imports
-## CALL_SUB_TUTORIAL setup
-## CALL_SUB_TUTORIAL basic_info
-## CALL_SUB_TUTORIAL plan_to_joint_state
-## CALL_SUB_TUTORIAL plan_to_pose
-## CALL_SUB_TUTORIAL plan_cartesian_path
-## CALL_SUB_TUTORIAL display_trajectory
-## CALL_SUB_TUTORIAL execute_plan
-## CALL_SUB_TUTORIAL add_box
-## CALL_SUB_TUTORIAL wait_for_scene_update
-## CALL_SUB_TUTORIAL attach_object
-## CALL_SUB_TUTORIAL detach_object
-## CALL_SUB_TUTORIAL remove_object
-## END_TUTORIAL
